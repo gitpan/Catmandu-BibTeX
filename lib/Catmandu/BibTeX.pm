e0370d87495568998929d35c3fbf88c96a1633e0
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2013-06-21 08:09:11 +0000

    Import of NICS/Catmandu-BibTeX-0.02 from CPAN.
    gitpan-cpan-distribution: Catmandu-BibTeX
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         NICS/Catmandu-BibTeX-0.02.tar.gz
    gitpan-cpan-author:       NICS
    gitpan-cpan-maturity:     released

---
+++ lib/Catmandu/BibTeX.pm
@@ -0,0 +1,41 @@
+package Catmandu::BibTeX;
+
+=head1 NAME
+
+Catmandu::BibTeX - Catmandu modules for working with BibTeX data
+
+=head1 VERSION
+
+Version 0.02
+
+=cut
+
+our $VERSION = '0.02';
+
+=head1 MODULES
+
+=over
+
+=item * L<Catmandu::Exporter::BibTeX>
+
+=back
+
+=head1 AUTHOR
+
+Nicolas Steenlant C<< <nicolas.steenlant at ugent.be> >>
+
+=head1 CONTRIBUTORS
+
+Vitali Peil C<< <vitali.peil at uni-bielefeld.de> >>
+
+=head1 LICENSE AND COPYRIGHT
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+=cut
+
+1;
