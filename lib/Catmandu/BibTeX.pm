25fdcd81afa78ad47d5742a97a8fc1d3eb0f75e0
Vitali Peil <vpeil@cpan.org>
2014-09-29 16:17:27 +0000

    Import of VPEIL/Catmandu-BibTeX-0.05 from CPAN.
    gitpan-cpan-distribution: Catmandu-BibTeX
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         VPEIL/Catmandu-BibTeX-0.05.tar.gz
    gitpan-cpan-author:       VPEIL
    gitpan-cpan-maturity:     released

--- lib/Catmandu/BibTeX.pm
+++ lib/Catmandu/BibTeX.pm
@@ -1,16 +1,26 @@
 package Catmandu::BibTeX;
 
+our $VERSION = '0.05';
+
+1;
+
+__END__
+
 =head1 NAME
 
 Catmandu::BibTeX - Catmandu modules for working with BibTeX data
 
-=head1 VERSION
+=head1 STATUS
 
-Version 0.04
+=begin HTML
 
-=cut
+<p>
+<a href="https://travis-ci.org/LibreCat/Catmandu-BibTeX"><img src="https://travis-ci.org/LibreCat/Catmandu-Catmandu-BibTeX.svg?branch=master" alt="build status" /></a>
+<a href="https://coveralls.io/r/LibreCat/Catmandu-Catmandu-BibTeX"><img src="https://coveralls.io/repos/LibreCat/Catmandu-Catmandu-BibTeX/badge.png?branch=master" alt="coverage status" /></a>
+<a href="http://cpants.cpanauthors.org/dist/Catmandu-Catmandu-BibTeX"><img src="http://cpants.cpanauthors.org/dist/Catmandu-Catmandu-BibTeX.png" alt="kwalitee score" /></a>
+</p>
 
-our $VERSION = '0.04';
+=end HTML
 
 =head1 MODULES
 
@@ -29,6 +39,7 @@ Nicolas Steenlant C<< <nicolas.steenlant at ugent.be> >>
 =head1 CONTRIBUTORS
 
 Vitali Peil C<< <vitali.peil at uni-bielefeld.de> >>
+Jakov Voss C<< <voss@gbv.de> >>
 
 =head1 LICENSE AND COPYRIGHT
 
@@ -39,5 +50,3 @@ by the Free Software Foundation; or the Artistic License.
 See http://dev.perl.org/licenses/ for more information.
 
 =cut
-
-1;
